950f7af43479f4a269360c063e19a231be6f4944
Lucas Cordó <lc2802@hotmail.com>
2026-03-17 18:18:44 -0300

    Cambios en repositorio

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HomeComponent } from "./components/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ContactComponent } from "./components/contact/contact.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [ProjectsComponent, FooterComponent, SkillsComponent, AboutComponent, HomeComponent, HeaderComponent, ContactComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent {
+  skills = [
+    { name: 'JavaScript', level: 80, icon: 'devicon-javascript-plain colored' },
+    { name: 'Java', level: 70, icon: 'devicon-java-plain colored' },
+    { name: 'TypeScript', level: 85, icon: 'devicon-typescript-plain colored' },
+    { name: 'SQL', level: 70, icon: 'devicon-postgresql-plain' },
+    { name: 'Docker', level: 70, icon: 'devicon-docker-plain' },
+    { name: 'Spring Boot', level: 70, icon: 'devicon-spring-plain colored' },
+    { name: 'OpenLayers', level: 95, icon: 'bi bi-layers' },
+    { name: 'Oauth2', level: 60, icon: 'devicon-oauth-plain' },
+    { name: 'Node.js', level: 70, icon: 'devicon-nodejs-plain colored' },
+    { name: 'Angular', level: 85, icon: 'devicon-angular-plain colored' },
+  ];
+
+getBorderProgress(level: number): string {
+  return `conic-gradient(#465565 ${level * 3.6}deg, transparent ${level * 3.6}deg)`;
+}
+
+}
