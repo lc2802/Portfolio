751e0184ba8ac9c00004fef429764cb2500ce17b
Lucas Cordó <lc2802@hotmail.com>
2026-03-18 21:20:20 -0300

    Refactor carousel
    Se cambio el carousel por un mockup y se arreglaron algunos estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from "./components/projects/projects.component";
-import { FooterComponent } from "./components/footer/footer.component";
 import { SkillsComponent } from "./components/skills/skills.component";
 import { AboutComponent } from "./components/about/about.component";
 import { HomeComponent } from "./components/home/home.component";
@@ -10,7 +8,7 @@ import { ContactComponent } from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ProjectsComponent, FooterComponent, SkillsComponent, AboutComponent, HomeComponent, HeaderComponent, ContactComponent],
+  imports: [ProjectsComponent, SkillsComponent, AboutComponent, HomeComponent, HeaderComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
